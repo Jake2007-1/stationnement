5f144858b2598147ac81c21202c29ccfd4a3e155
Jake2007-1 <jacobdury2007@gmail.com>
2025-10-17 15:40:47 -0400

    ajout paiement carte de credit

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,7 +1,8 @@
-
+import java.util.Scanner;
 
 public class Transaction {
     private int duree;
+    private int tarif;
     private int montant;
     private String zone;
 
@@ -15,10 +16,41 @@ public class Transaction {
 
   public Transaction(String zone){
       this.zone = zone;
+      tarif = zone == "SQ" ? 225 : 425;
+      duree = 0;
+      montant = 0;
   }
 
   public void paiement(Credit carte){
       if(carte.validCarte()){
+          Scanner s = new Scanner(System.in);
+          boolean confirmed = false;
+          while(!confirmed || duree == 120){
+              if (s.nextLine() == "+"){
+                  duree += 15;
+                  montant += tarif / 4;
+
+              }
+              else if (s.nextLine() == "-" && duree != 0){
+                  duree -= 15;
+                  montant -= tarif / 4;
+
+              }
+              else if( s.nextLine() == "max"){
+                  duree = 120;
+                  montant = tarif * 2;
+              }
+              else if (s.nextLine() == "confirm") {
+                  confirmed = true;
+              }
+              else if (s.nextLine() == "cancel") {
+                  duree = 0;
+                  montant = 0;
+                  confirmed = true;
+              }
+              System.out.println("Duree : " + duree / 100);
+              System.out.println("Montant : " + montant / 100);
+          }
 
       }
       else {
